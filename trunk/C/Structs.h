32bf55d7ca7b5f0bb61dc4b2fbb5f526bc0593ee
luigugah <luigugah@bfc883cd-d670-24a0-41ac-96accb49dfb3>
2012-11-11 15:42:20 +0000

    Definidas as structs que farao parte de todos os programas (Sujeito a alteracoes)

---
+++ trunk/C/Structs.h
@@ -0,0 +1,37 @@
+#ifndef STRUCTS_H_INCLUDED
+#define STRUCTS_H_INCLUDED
+
+struct Endereco{
+    char rua[40];
+    char numero[20];
+    int cep;
+    char complemento[40];
+};
+
+struct DataDeNascimento{
+    int dia, mes, ano;
+};
+
+struct Telefones{
+    int telefone1,telefone2,celular1, celular2;
+};
+
+struct PessoaFisica{
+    char nome[40];
+    int RG, CPF;
+    struct Telefones telefone;
+    struct DataDeNascimento nascimento;
+    struct Endereco endereco, enderecoComercial;
+};
+
+struct PessoaJuridica{
+
+    char nomeFantasia[40], rasaoSocial[60];
+    int CNPJ;
+    struct DataDeNascimento nascimento;
+    struct Endereco endereco;
+    struct Telefones telefone;
+};
+
+
+#endif // STRUCTS_H_INCLUDED
